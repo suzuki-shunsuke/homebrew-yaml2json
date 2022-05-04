ccb4dab9faa9894c4ebac41de3a99a83eb0cdb00
goreleaserbot <goreleaser@carlosbecker.com>
2022-05-04 22:05:04 +0000

    Brew formula update for yaml2json version v0.2.0

--- yaml2json.rb
+++ yaml2json.rb
@@ -6,21 +6,21 @@ class Yaml2json < Formula
   desc "Convert YAML to JSON
 "
   homepage "https://github.com/suzuki-shunsuke/yaml2json"
-  version "0.1.1"
+  version "0.2.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/suzuki-shunsuke/yaml2json/releases/download/v0.1.1/yaml2json_darwin_arm64.tar.gz"
-      sha256 "f75b22927e7d7db34b0175dac29782bdd3b2878d36cdef04c5efabb11e947b19"
+      url "https://github.com/suzuki-shunsuke/yaml2json/releases/download/v0.2.0/yaml2json_darwin_arm64.tar.gz"
+      sha256 "cfeb1d915aa759301869965a8ede959ff827446400bd41191bf6295d2c4e7432"
 
       def install
         bin.install "yaml2json"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/suzuki-shunsuke/yaml2json/releases/download/v0.1.1/yaml2json_darwin_amd64.tar.gz"
-      sha256 "4e186821291b22c4989158f2d4f25d4429312a76920a5b92009527cb32b3b79f"
+      url "https://github.com/suzuki-shunsuke/yaml2json/releases/download/v0.2.0/yaml2json_darwin_amd64.tar.gz"
+      sha256 "6bceff0cf4290bfb600fffb318160a8851f01aa6d1a1dd4b0a82425d7b9ae6b7"
 
       def install
         bin.install "yaml2json"
@@ -29,17 +29,17 @@ class Yaml2json < Formula
   end
 
   on_linux do
-    if Hardware::CPU.intel?
-      url "https://github.com/suzuki-shunsuke/yaml2json/releases/download/v0.1.1/yaml2json_linux_amd64.tar.gz"
-      sha256 "58e7857180276e85f2fc04f5780ab86b86202834db6198cae8ef388cc9293a2c"
+    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+      url "https://github.com/suzuki-shunsuke/yaml2json/releases/download/v0.2.0/yaml2json_linux_arm64.tar.gz"
+      sha256 "963fb55372c83dd206156dafe7ff2577643e2112736139ce12f7609555ea0533"
 
       def install
         bin.install "yaml2json"
       end
     end
-    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/suzuki-shunsuke/yaml2json/releases/download/v0.1.1/yaml2json_linux_arm64.tar.gz"
-      sha256 "1b3bf9e7c01ae71f52d10e9993ef3ed2258f1590d4c89805d9711b5161cc1af7"
+    if Hardware::CPU.intel?
+      url "https://github.com/suzuki-shunsuke/yaml2json/releases/download/v0.2.0/yaml2json_linux_amd64.tar.gz"
+      sha256 "d82147d45efc4b9891904121111509856fb1c93cb2798febf46dee671a04f9d6"
 
       def install
         bin.install "yaml2json"
